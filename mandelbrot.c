ad5ba6178c8ff4f286c0d87a6099e48522ea8e1d
Rémy Oudompheng <remy@archlinux.org>
2011-11-11 17:18:41 +0100

    Animated version of mandelbrot example.

--- mandelbrot.c
+++ mandelbrot.c
@@ -3,7 +3,7 @@
 int test(c1, c2) double c1, c2; {
   double x, y;
   x = y = 0;
-  for(int n = 0; n < 16; n++) {
+  for(int n = 0; n < 1000; n++) {
     double newx = x*x - y*y + c1;
     double newy = 2*x*y + c2;
     x = newx; y = newy;
@@ -14,17 +14,31 @@ int test(c1, c2) double c1, c2; {
   return 1;
 }
 
+double refx = .44;
+double refy = .374;
+
 int main() {
   char buffer[2048];
   int pos = 0;
-  for(int i = 0; i < 25; i++) {
+  double scale = 2;
+start:
+  pos = 0;
+  // Erase screen
+  buffer[pos++] = '\e';
+  buffer[pos++] = '[';
+  buffer[pos++] = '2';
+  buffer[pos++] = 'J';
+  for(int i = 0; i < 30; i++) {
     for(int j = 0; j < 80; j++) {
-      double x = (j - 40.0) / 20;
-      double y = (i - 12.0) / 10;
+      double x = refx + (j - 40.0) / 20.0 * scale;
+      double y = refy + (i - 15.0) / 10.0 * scale;
       buffer[pos++] = test(x,y) ? '*' : ' ';
     }
     buffer[pos++] = '\n';
   }
   write(1, buffer, pos);
+  usleep(100000);
+  scale *= 0.98;
+  goto start;
   return 0;
 }
